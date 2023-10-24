567478d7dd0b64bf20909198640d9f8b0f6b5204
Codigo33 <codigotreintaytres33@gmail.com>
2023-10-24 01:39:06 -0300

    headr

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 /*===== MENU SHOW =====*/ 
+const overlay = document.getElementById('nav-overlay');
+
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId);
     const nav = document.getElementById(navId);
 
-    if (toggle && nav) {
+    if (toggle && nav && overlay) {
         toggle.addEventListener('click', ()=> {
-            nav.classList.toggle('show')
-        })
+            nav.classList.toggle('show');
+            overlay.style.display = nav.classList.contains('show') ? 'block' : 'none';
+        });
     }
 }
 showMenu('nav-toggle', 'nav-menu');
@@ -16,11 +19,19 @@ const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
     // Active Link
-    navLink.forEach(n => n.classList.remove('active'))
-    this.classList.add('active')
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
 
     // Remove menu mobile
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show')
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
+    overlay.style.display = "none";
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/* ===== REMOVE MENU FROM OVERLAY ===== */
+overlay.addEventListener('click', () => {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
+    overlay.style.display = "none";
+})
